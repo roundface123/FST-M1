4d8f9e57df71d0e92ce713ef7fcb506bbfe446cf
roundface123 <mailmebysaro@gmail.com>
2025-09-04 13:02:22 +0530

    Create Activity1.java

---
+++ Appium/Activities/Activity1.java
@@ -0,0 +1,64 @@
+package activities;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class Activity1 {
+	
+	AppiumDriver driver;
+	WebDriverWait wait;
+	
+	//Setup function
+	@BeforeClass
+	public void setup() throws MalformedURLException, URISyntaxException {
+		//define the capabilities
+		UiAutomator2Options options = new UiAutomator2Options();
+		options.setPlatformName("android");
+		options.setAutomationName("UiAutomator2");
+//		options.setApp("path/to/application.apk");
+		options.setApp("C:/Users/AshaAnnaThomas/eclipse-workspace/FST_Appium/src/test/resources/Calculator.apk");
+		
+		//Only for android
+//		options.setAppPackage("com.google.android.calculator");
+//		options.setAppActivity("com.android.calculator2.Calculator");
+		options.noReset();
+		
+		//set the appium server url
+		URL serverurl = new URI("http://localhost:4723").toURL();
+		
+		//initialize the driver
+		driver = new AndroidDriver(serverurl, options);
+	}
+	
+	@Test
+	public void multiplyTest() {
+		
+		driver.findElement(AppiumBy.id("digit_5")).click();
+		driver.findElement(AppiumBy.accessibilityId("multiply")).click();
+		driver.findElement(AppiumBy.id("digit_8")).click();
+		driver.findElement(AppiumBy.accessibilityId("equals")).click();
+		
+		String result = driver.findElement(AppiumBy.id("result_final")).getText();
+		
+		Assert.assertEquals("40", result);
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
